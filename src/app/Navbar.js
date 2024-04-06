4d577dc808efab2e4e4e647ad4ed9c8671357629
MunaizAhmed <munaiz@munaizahmed.com>
2024-04-06 03:44:57 -0500

    Second commit ->  got a good base down for frontend. Updated font family, added base collections page where tokensor or ordinals will be rendered. Next: Setup backend in aws

---
+++ src/app/Navbar.js
@@ -0,0 +1,32 @@
+"use client"
+import React from 'react';
+import Link from 'next/link';
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+
+const navItems = [
+  {page:"Munaiz Ahmed", route:'/'},
+  {page:"Quantum Cats", route:'/quantum_cats'},
+  {page:"Ordinal Maxi Biz", route:'/omb'},
+]
+
+export default function NavBar() {
+
+
+  return (
+      <AppBar sx={{bgcolor: 'inherit'}} position="fixed">
+        <Toolbar >
+            {navItems.map((item,i) =>(
+            <Button key={i} color="inherit" sx={{fontFamily:'monospace', letterSpacing: '.1rem'}}>
+                <Link href={item.route}>{item.page}</Link>
+            </Button>
+            ))}
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1}}></Typography>
+        
+        </Toolbar>
+      </AppBar>
+  );
+}
---
+++ src/app/[collectionName]/page.js
@@ -0,0 +1,23 @@
+'use client'
+import styles from "../page.module.css";
+import Grid from '@mui/material/Grid';
+import React, { useEffect, useState} from 'react'
+
+export default function CollectionPage({params}) {
+    const [header, setHeader] = useState('')
+
+    useEffect(() => {
+        setHeader(params.collectionName === 'omb' ? "Ordinal Maxi Biz (OMB)" : "Quantum Cats")
+    })
+
+
+  return (
+    <div className={styles.main}>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <h2 className={styles.title}>{header}</h2>
+            </Grid>
+          </Grid>
+    </div>
+  );
+}
